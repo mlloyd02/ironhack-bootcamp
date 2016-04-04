81d2f64bec057ecadc211aa6d0f14f846c96a643
mlloyd02 <mlloyd02@gmail.com>
2016-04-04 10:16:41 -0500

    update Lex and add toDoList

--- week2/lexiconomitron/lexiconomitron_rspec.rb
+++ week2/lexiconomitron/lexiconomitron_rspec.rb
@@ -1,26 +1,32 @@
 require "rspec"
 require "./lexiconomitron"
 
-RSpec.describe "#eat_t" do
+describe Lexiconomitron do
 
 	before :each do
 		@lexi = Lexiconomitron.new
 	end
 
-  it "removes every letter t from the input" do
-    expect(@lexi.eat_t("great scott!")).to eq("grea sco!")
+  describe "eat_t" do
+    it "removes every letter t from the input" do
+      expect(@lexi.eat_t("great scott!")).to eq("grea sco!")
+    end
   end
 
-  it "reverses the letters and returns first and last strings in an array" do
-  	expect(@lexi.shazam(["This", "is", "a", "boring", "test"])).to eq(["sih", "se"])
+  describe "shazam" do
+    it "reverses the letters and returns first and last strings in an array" do
+  	 expect(@lexi.shazam(["This", "is", "a", "boring", "test"])).to eq(["sih", "se"])
+    end
   end
 
-  it "returns an array with only words containing three or fewer characters" do
-  	expect(@lexi.oompa(["if", "you", "wanna", "be", "my", "lover"])).to eq(["if", "you", "be", "my"])
-  end
+  describe "oompa" do
+    it "returns an array with only words containing three or fewer characters" do
+  	 expect(@lexi.oompa(["if", "you", "wanna", "be", "my", "lover"])).to eq(["if", "you", "be", "my"])
+    end
 
-  it "returns words with fewer than three characters, deleting all t's" do
-  	expect(@lexi.oompa(["if", "tot", "kit", "to", "my", "lover"])).to eq(["if", "o", "ki", "o", "my"])
+    it "returns words with fewer than three characters, deleting all t's" do
+  	 expect(@lexi.oompa(["if", "tot", "kit", "to", "my", "lover"])).to eq(["if", "o", "ki", "o", "my"])
+    end
   end
 
 end	
